c232f5df0de3a6dc9734c7e8f4b08328b18b57fa
zmx21 <zhi.xu@epfl.ch>
2023-07-17 16:57:58 +0200

    shiny launch check

--- workflow/launch_shiny.sh
+++ workflow/launch_shiny.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+FILE="$(pwd)"/result/G2G_Results.bmat
+
+if ! [[ $(stat -c "%A" $FILE) =~ "r" ]]; then
+  echo "Please add read permission: chmod a+r ./results/G2G_Results.*"
+fi
+
 docker run --rm --publish 3838:3838 --mount type=bind,source="$(pwd)"/results,target=/home/results zmxu/g2g_shiny_docker
