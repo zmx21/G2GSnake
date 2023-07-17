b04f5cd9df96b453fa7d8676b2a873bf4104b182
zmx21 <zhi.xu@epfl.ch>
2023-07-17 17:02:39 +0200

    launch script bug fix

--- workflow/run_snakemake.sh
+++ workflow/run_snakemake.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-N_Cores=$0
+N_Cores=$1
 snakemake --use-singularity --cores $N_Cores --rerun-incomplete --singularity-args "-B ./results:/home/results,../raw_data:/raw_data,./scripts:/home/scripts"
