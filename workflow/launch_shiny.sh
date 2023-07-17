10fc50ca31512d99f699f063d1e48003def625e7
zmx21 <zhi.xu@epfl.ch>
2023-07-18 00:11:24 +0200

    launch shiny fix

--- workflow/launch_shiny.sh
+++ workflow/launch_shiny.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 FILE="$(pwd)"/results/G2G_Results.bmat
 
-file_permission=$(stat -f "%A" $FILE)
+file_permission=$(stat -c "%a" $FILE)
 third_digit=$((file_permission % 10))
-
 
 if [[ $third_digit -lt 4 ]]; then
   echo "Please add read permission: chmod a+r ./results/G2G_Results.*"
