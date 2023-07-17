f58d5c37d5e1af648a3b9ee134b2dad3588f76ae
Zhi Ming Xu <Zhi Ming Xu>
2023-07-17 23:58:37 +0200

    launch shiny fix

--- workflow/launch_shiny.sh
+++ workflow/launch_shiny.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 FILE="$(pwd)"/results/G2G_Results.bmat
 
-if ! [[ $(stat -c "%A" $FILE) =~ "r" ]]; then
+file_permission=$(stat -f "%A" $FILE)
+third_digit=$((file_permission % 10))
+
+
+if [[ $third_digit -lt 4 ]]; then
   echo "Please add read permission: chmod a+r ./results/G2G_Results.*"
   exit 0
 fi
